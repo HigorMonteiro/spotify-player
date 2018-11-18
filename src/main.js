8319900245ae8270103487f2c49d507d0ade9454
HigorVinicius <higor.tecinfor@gmail.com>
2018-11-18 02:25:27 -0300

    Add AlbumInfo no layout

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
 
 const albums = spotify.search.albums('Projota');
-const albumList = document.getElementById('albums-list')
+const albumList = document.getElementById('albums-list');
 
-albums
-  .then(data => renderAlbums(data.albums.items, albumList));
+const album = spotify.album.getAlbum("2TsL9NFMTknpXO7P962oyj");
+const albumInfo = document.getElementById('album-info');
+
+albums.then(data => renderAlbums(data.albums.items, albumList));
+album.then(data => renderAlbumInfo(data, albumInfo));
 
 
 albums.then(data => console.log(data.albums.items, albumList));
