33fb3f021ada9922331dc43012f979908c2dbc4b
HigorVinicius <higor.tecinfor@gmail.com>
2018-11-19 13:01:53 -0300

    create method convert to human time

---
+++ src/ConvertToHumanTime.js
@@ -0,0 +1,10 @@
+export default function convertToHumanTime(durantion) {
+  let s = parseInt((durantion / 1000) % 60, 10);
+  const m = parseInt((durantion / (1000 * 60)) % 60, 10);
+
+  s = (s < 10) ? `0${s}` : s;
+
+  return `${m}:${s}`
+}
+
+
