cb0d8757f5123d2e45ecaa11778c9edf9a7d6ac7
HigorVinicius <higor.tecinfor@gmail.com>
2018-11-19 17:10:47 -0300

    create select Album

--- src/Spotify.js
+++ src/Spotify.js
@@ -2,7 +2,7 @@ import SpotifyWrapper from 'spotify-wrapper-es6'
 
 const spotify = new SpotifyWrapper({
   token:
-  'BQDmh8jUC688jza5pr1qnQEKvCfp1Zhbb8Gq5lZFDkiQlO7FDUzXlXPqZHiCPG8MTkuCCxbBI6hXM12XeXmVeup4CsCZsVYDGPxfpF0DNkFuTAb8Ih-4UL-2ihPOMxCoXngwsN4EbJceES7XsUeu_ZfbrHYv_G5STv5nNg7A2myC2OlEFkNSSbyz8W9qWFaRytFhR-ddqGzc'
+  'BQCY6Blq-O5KaGAzH5ee6THERnvNH9YkZd3NE3cBz7g6Y_e694sGfW97zRgkiVp_nFwRRWVhRYKiLR03l7yPi92kKpST54gk5Rx7jsejA2mdUGoZrZuJ__CQpfiySGM9R1tXBUb8bAfT7QhOzR2o8oe2cdF1n1Ocs_HNlQ0a8PmKv2pCStTJjVJ_DiHIeDFfERO_msnmGQyg'
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,22 +1,17 @@
 import spotify from './Spotify';
-import renderAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
 import renderAlbumTracks from './AlbumTracks';
+import searchEnterTrigger from './SearchTrigger';
 
-
-const albums = spotify.search.albums('Projota');
-const albumList = document.getElementById('albums-list');
+searchEnterTrigger();
 
 const album = spotify.album.getAlbum("2TsL9NFMTknpXO7P962oyj");
 const albumInfo = document.getElementById('album-info');
 const albumTracks = document.getElementById('album-tracks');
 
 
-albums.then(data => renderAlbums(data.albums.items, albumList));
-
 album
   .then(data => renderAlbumInfo(data, albumInfo))
   .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
 
-albums.then(data => console.log(data.albums.items, albumList));
 
