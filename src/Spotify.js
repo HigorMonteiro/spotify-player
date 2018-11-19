fc8b0871e46a41f076e36d96f704b2459df35c73
HigorVinicius <higor.tecinfor@gmail.com>
2018-11-19 12:37:07 -0300

    create components AlbumsTracks

--- src/Spotify.js
+++ src/Spotify.js
@@ -2,7 +2,7 @@ import SpotifyWrapper from 'spotify-wrapper-es6'
 
 const spotify = new SpotifyWrapper({
   token:
-  'BQCsBqcW6z3jV2k8gbrPnlpnVv-I9Lb11HwRCt10QKub1Lxch8OOz1XhVYXKtZ-svEZYesgepspZMefjXnY-uY_CLgDoBj89_hwJ0Tqecq_OBGzAxS_y7-HFh8Yn6jzTUddAtW-3d1ksVETLLEHgh766V3cVG2kqTxmzkVURvRumSVraEoyZtSbr_5ZO-d0H11JX_yOcKRKw'
+  'BQDmh8jUC688jza5pr1qnQEKvCfp1Zhbb8Gq5lZFDkiQlO7FDUzXlXPqZHiCPG8MTkuCCxbBI6hXM12XeXmVeup4CsCZsVYDGPxfpF0DNkFuTAb8Ih-4UL-2ihPOMxCoXngwsN4EbJceES7XsUeu_ZfbrHYv_G5STv5nNg7A2myC2OlEFkNSSbyz8W9qWFaRytFhR-ddqGzc'
 });
 
 export default spotify;
